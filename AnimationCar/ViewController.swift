60b868d3e0390c7cdee7b399be93cb30a64e8514
grangegermi <dasha.voloshina@gmail.com>
2022-09-23 23:19:53 +0300

    Initial Commit

---
+++ AnimationCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationCar
+//
+//  Created by Даша Волошина on 23.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
